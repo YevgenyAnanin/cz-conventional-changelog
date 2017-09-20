019dd7cfc2b0c162cf978c32913d5adc2e08c91b
Yevgeny Ananin <euug0701@gmail.com>
2017-09-20 09:13:02 -0500

    added new commit type of theming.

--- index.js
+++ index.js
@@ -2,7 +2,12 @@
 
 var engine = require('./engine');
 var conventionalCommitTypes = require('conventional-commit-types');
+var types = conventionalCommitTypes.types;
+types.theming = {
+  description: "Updates to CSS or HTML markup",
+  title: "Theming"
+};
 
 module.exports = engine({
-  types: conventionalCommitTypes.types
+  types: types
 });
